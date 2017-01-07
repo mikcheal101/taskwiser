b4b6e1aa448ae131f35067695b170cf5980f3fd9
hirekaanmicheal@gmail.com <hirekaanmicheal@gmail.com>
2017-01-07 22:22:05 +0100

    update Jan 7 10:22

--- application/config/email.php
+++ application/config/email.php
@@ -1,13 +1,16 @@
 <?php
 
-$config['protocol']			= "smtp";
-$config['smtp_host']		= "smtp.office365.com";
-$config['smtp_port']		= "587"; # 3535 or 80 or 25
-$config['smtp_user']		= "no-reply@taskwiser.com";
-$config['smtp_pass']		= "!QAZ2wsx";
 $config['mailtype']			= "html";
-$config['charset']			= "UTF-8";
+$config['smtp_port']		= "587"; # 3535 or 80 or 25
+$config['smtp_timeout']		= '30';
+#$config['charset']			= "utf-8";
+$config['charset'] 			= 'iso-8859-1';
+$config['protocol']			= "smtp";
+$config['mailpath'] 		= '/usr/sbin/sendmail';
 $config['wordwrap']			= TRUE;
 $config['newline']			= "\r\n";
 $config['crlf']				= "\r\n";
-?>
+
+$config['smtp_host']		= "smtp.office365.com";
+$config['smtp_user']		= "no-reply@taskwiser.com";
+$config['smtp_pass']		= "!QAZ2wsx";
