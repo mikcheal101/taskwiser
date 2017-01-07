6da052382c8b97fce519af001d2432b5dc5b18bd
hirekaanmicheal@gmail.com <hirekaanmicheal@gmail.com>
2017-01-07 22:09:08 +0100

    update Jan 7 10:09

--- application/config/email.php
+++ application/config/email.php
@@ -1,10 +1,10 @@
 <?php
 
 $config['protocol']			= "smtp";
-$config['smtp_host']		= "smtp.sendgrid.net";
+$config['smtp_host']		= "smtp.office365.com";
 $config['smtp_port']		= "587"; # 3535 or 80 or 25
-$config['smtp_user']		= "app61611216@heroku.com";
-$config['smtp_pass']		= "qyk27qp78475";
+$config['smtp_user']		= "no-reply@taskwiser.com";
+$config['smtp_pass']		= "!QAZ2wsx";
 $config['mailtype']			= "html";
 $config['charset']			= "UTF-8";
 $config['wordwrap']			= TRUE;
